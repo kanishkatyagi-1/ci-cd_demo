f1c1693c893cf5604e173fa310c73b3fdbfc35da
Kanishka <tkanishka589@gmail.com>
2026-04-09 16:50:35 +0530

    more changes

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/',(req,res)=>{
     res.send("hello world")
 })
 app.listen(5000,()=>{
-    console.log("Server is runnning on port 5000. ")
+    console.log("Server is runnning on port 5000.New changes made to the file ")
 })
 
