1f3449a5f8a7e03ebbfd3e373cb2bb5808c51237
Kanishka <tkanishka589@gmail.com>
2026-04-09 16:39:46 +0530

    Initial setups

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+app.listen(5000,()=>{
+    console.log("Server is runnning on port 5000. This is a new file")
+})
+
